ff4763081ad001756e28a0b680001fe49ac6ff4a
Larry Botha <larry@fixate.it>
2018-01-02 10:47:12 +0200

    remove comments

--- 07-organise-code-by-subpath/index.js
+++ 07-organise-code-by-subpath/index.js
@@ -8,20 +8,11 @@ const app = express();
 
 let users = [];
 
-// this is a route handler we're using to verify if a user exists, and handle
-// the cases where one doesn't exist
 const verifyUser = (req, res, next) => {
   const fp = getUserFilePath(req.params.username);
 
-  // fs.exists is deprecated, use fs.access to determine whether the file exists
-  // or not
   fs.access(fp, err => {
     if (err) {
-      // we can indicate that the resulting callbacks where this callback resides
-      // not be called, and rather go on to another route
-      // next('route');
-
-      // but instead we can redirect to an error page with something more descriptive
       res.redirect(`/error/${req.params.username}`);
     } else {
       next();
@@ -95,37 +86,21 @@ app.get('/', (req, res) => {
   });
 });
 
-// We can define routes which only return json
-// This is useful when building APIs
 app.get('/data/:username', (req, res) => {
   const user = getUser(req.params.username);
 
   res.json(user);
 });
 
-// this route handler checks to see if there is a json extension, after which we
-// can use res.download which the client will download
 app.get('*.json', (req, res) => {
-  // this will download the file as is
   res.download(`${__dirname}/users/${req.path}`);
-
-  // this will specify a filename for download
-  // res.download(`${__dirname}/users/${req.path}`, 'my-filename.ext');
 });
 
-// we can monitor all requests to /:username
 app.all('/:username', (req, res, next) => {
-  // and log the request and params
   console.log(req.method, 'for', req.params.username);
-  // passing the request onto the next handler
   next();
 });
 
-// app.get takes as many callbacks as we wish
-// verifyUser here intercepts the next callback
-// It can prevent the request from continuing to the next callback by calling
-// next('route')
-// or it can have the request passed through by calling next() on its own
 app.get('/:username', verifyUser, (req, res) => {
   const user = getUser(req.params.username);
 
@@ -135,11 +110,7 @@ app.get('/:username', verifyUser, (req, res) => {
   });
 });
 
-// this is where we can indicate that a request for a specific user can't return
-// anything
 app.get('/error/:username', (req, res) => {
-  // we indicate that this is a 404, and we provide some information to render
-  // to indicate which user can't be found
   res.status(404).send(`user with username: ${req.params.username} not found`);
 });
 
